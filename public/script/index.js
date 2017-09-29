baafab594d754e8636ef6f0dafea6c3b668b7cde
Robin GUERIN <robin.guerin@numen.fr>
2017-09-29 17:01:17 +0200

    add texture wall

--- public/script/index.js
+++ public/script/index.js
@@ -4,8 +4,8 @@ if (!PIXI.utils.isWebGLSupported()) {
 }
 PIXI.utils.sayHello(type)
 
-const HEIGHT = 1000,
-    WIDTH = 1000
+const HEIGHT = 250,
+    WIDTH = 500
 
 //Aliases
 const Container = PIXI.Container,
@@ -19,53 +19,46 @@ const Container = PIXI.Container,
 //Create a Pixi stage and renderer and add the 
 //renderer.view to the DOM
 var stage = new Container(),
-    renderer = autoDetectRenderer(1000, 1000);
+    renderer = autoDetectRenderer(WIDTH, HEIGHT);
 document.body.appendChild(renderer.view);
 
-//Use Pixi's built-in `loader` object to load an image
 PIXI.loader
     .add([
-        "public/assets/images/rogue.png"
+        "public/assets/images/dunjo_cover.png"
     ])
     .on("progress", loadProgressHandler)
     .load(setup);
 
 function loadProgressHandler(loader, resource) {
-    //Display the file `url` currently being loaded
     console.log("loading: " + resource.url);
-
-    //Display the precentage of files currently loaded
     console.log("progress: " + loader.progress + "%")
-
-    //If you gave your files names as the first argument 
-    //of the `add` method, you can access them like this
-    //console.log("loading: " + resource.name);
 }
 
 createjs.Sound.registerSound("public/assets/sound/laser/laser1.wav", "laser");
 
-//This `setup` function will run when the image has loaded
 function setup() {
-    //Create the `cat` sprite, add it to the stage, and render it
-    //const rogue = new Sprite(resources["public/assets/images/rogue.png"].texture);
-    const texture = TextureCache["public/assets/images/rogue.png"];
 
-    const rectangle = new Rectangle(0, 0, 300, 300);
+    const texture = TextureCache["public/assets/images/dunjo_cover.png"];
+    let wall;
 
-    //Tell the texture to use that rectangular section
-    texture.frame = rectangle;
+    const top_right_corner = new Rectangle(43, 75, 25, 25);
+    wall = new Sprite(texture);
+    texture.frame = top_right_corner;
+    stage.addChild(wall)
 
-    //Create the sprite from the texture
-    let rogue = new Sprite(texture);
+    const right_wall = new Rectangle(86, 75, 25, 25);
+    wall = new Sprite(texture);
+    wall.x = 0
+    wall.y = 25
+    texture.frame = right_wall;
+    stage.addChild(wall)
 
-    //Change the sprite's position
-    rogue.x = HEIGHT / 2
-    rogue.y = WIDTH / 2
-    //Change the sprite's size
-    rogue.scale.x = 4
-    rogue.scale.y = 4
-    //add sprit to view
-    stage.addChild(rogue)
+    // rogue.x = HEIGHT / 2
+    // rogue.y = WIDTH / 2
+    // rogue.scale.x = 2
+    // rogue.scale.y = 2
+
+    //stage.addChild(wall)
     renderer.render(stage)
 
     createjs.Sound.play("laser");

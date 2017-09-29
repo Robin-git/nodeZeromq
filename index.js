c730416c3ac27ab0d118cf2bb664730e8aa5a475
Robin GUERIN <robin.guerin@numen.fr>
2017-09-29 14:34:41 +0200

    add sound and socket

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require('express'),
     app = express(),
+    http = require('http').Server(app),
     path = require('path'),
-    engines = require('consolidate')
+    engines = require('consolidate'),
+    io = require('socket.io')(http)
 
 app.use('/public', express.static('public'))
 app.use('/pixi', express.static(__dirname + '/node_modules/pixi.js/dist/'))
+app.use('/socket.io', express.static(__dirname + '/node_modules/socket.io-client/dist/'))
 
-app.engine('html', engines.mustache);
+app.engine('html', engines.mustache)
 
-app.set('view engine', 'html');
+app.set('view engine', 'html')
 app.set('views', __dirname + '/public/html')
 
 app.get('/', function (req, res) {
     res.render('index')
 })
 
-app.listen(8000, function () {
-    console.log('Example app listening on port 8000!')
+io.on('connection', function (socket) {
+    console.log('a user connected')
+    socket.on('disconnect', function () {
+        console.log('user disconnected')
+    })
+})
+
+http.listen(8000, function () {
+    console.log('listening on *:8000');
 })
--- public/script/index.js
+++ public/script/index.js
@@ -1,11 +1,72 @@
-//Create the renderer
-var renderer = PIXI.autoDetectRenderer(256, 256);
+var type = "WebGL"
+if (!PIXI.utils.isWebGLSupported()) {
+    type = "canvas"
+}
+PIXI.utils.sayHello(type)
 
-//Add the canvas to the HTML document
+const HEIGHT = 1000,
+    WIDTH = 1000
+
+//Aliases
+const Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite,
+    TextureCache = PIXI.TextureCache,
+    Rectangle = PIXI.Rectangle
+
+//Create a Pixi stage and renderer and add the 
+//renderer.view to the DOM
+var stage = new Container(),
+    renderer = autoDetectRenderer(1000, 1000);
 document.body.appendChild(renderer.view);
 
-//Create a container object called the `stage`
-var stage = new PIXI.Container();
+//Use Pixi's built-in `loader` object to load an image
+PIXI.loader
+    .add([
+        "public/assets/images/rogue.png"
+    ])
+    .on("progress", loadProgressHandler)
+    .load(setup);
 
-//Tell the `renderer` to `render` the `stage`
-renderer.render(stage);
+function loadProgressHandler(loader, resource) {
+    //Display the file `url` currently being loaded
+    console.log("loading: " + resource.url);
+
+    //Display the precentage of files currently loaded
+    console.log("progress: " + loader.progress + "%")
+
+    //If you gave your files names as the first argument 
+    //of the `add` method, you can access them like this
+    //console.log("loading: " + resource.name);
+}
+
+createjs.Sound.registerSound("public/assets/sound/laser/laser1.wav", "laser");
+
+//This `setup` function will run when the image has loaded
+function setup() {
+    //Create the `cat` sprite, add it to the stage, and render it
+    //const rogue = new Sprite(resources["public/assets/images/rogue.png"].texture);
+    const texture = TextureCache["public/assets/images/rogue.png"];
+
+    const rectangle = new Rectangle(0, 0, 300, 300);
+
+    //Tell the texture to use that rectangular section
+    texture.frame = rectangle;
+
+    //Create the sprite from the texture
+    let rogue = new Sprite(texture);
+
+    //Change the sprite's position
+    rogue.x = HEIGHT / 2
+    rogue.y = WIDTH / 2
+    //Change the sprite's size
+    rogue.scale.x = 4
+    rogue.scale.y = 4
+    //add sprit to view
+    stage.addChild(rogue)
+    renderer.render(stage)
+
+    createjs.Sound.play("laser");
+}
